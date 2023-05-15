da8fd222bbde582f018b84eedac06b137a831038
MS929 <dri2718@naver.com>
2023-05-15 19:44:30 +0900

    Work in progress..

---
+++ src/pages/pay.tsx
@@ -0,0 +1,54 @@
+import { loadTossPayments } from "@tosspayments/payment-sdk";
+import "./pay.scss";
+import { useEffect } from "react";
+
+const ClinetKey = 'test_ck_MGjLJoQ1aVZZjvmO6BwVw6KYe2RN'
+
+loadTossPayments(ClinetKey).then(TossPayments => {
+    TossPayments.requestPayment('카드', {
+        amount: 100,
+        orderId: 'Hello',
+        orderName: '테스트 결제',
+        customerName: '테스트',
+        successUrl: 'http://localhost:3000/success',
+        failUrl: 'http://localhost:3000/fail',
+    })
+        .catch(function (error) {
+            if (error.code == 'USER_CANCEL') {
+            } else if (error.code === 'INVALID_CARD_COMPANY') {
+
+            }
+        });
+    TossPayments.requestPayment('가상계좌', {
+
+        amount: 15000,
+        orderId: 'YBvN8NKG9orTghwOYZgOP',
+        orderName: '토스 티셔츠 외 2건',
+        customerName: '박토스',
+        successUrl: 'http://localhost:3000/success',
+        failUrl: 'http://localhost:3000/fail',
+        validHours: 24,
+        cashReceipt: {
+            type: '소득공제',
+        },
+    })
+        .catch(function (error) {
+            if (error.code == 'USER_CANCEL') {
+            } else if (error.code === 'INVALID_CARD_COMPANY') {
+
+            }
+        });
+
+    TossPayments.requestBillingAuth('카드', {
+        customerKey: 'oaS3PTG3g9MnHAxbyu58d',
+        successUrl: 'http://localhost:3000/success',
+        failUrl: 'http://localhost:3000/fail',
+    })
+
+    useEffect(() => {
+        return () => {
+            TossPayments.cancelPayment();
+        };
+    }, [TossPayments]);
+
+});
