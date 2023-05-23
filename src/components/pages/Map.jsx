f09c48160e93146cd20147cd0987fdfe92b09428
rainbow5079 <rlaxogh507906@gmail.com>
2023-05-23 18:25:21 +0900

    Fix map loading error

--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -1,9 +1,10 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk";
-import { useState, useEffect } from "react";
 import "./styles/Map.scss";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState } from "react";
+import Loading from "../tools/MovieLoading";
 
 const BeautifulMap = () => {
-  const [location, setLocation] = useState({});
+  const [location, setLocation] = useState({ lat: 0, long: 0 });
   navigator.geolocation.getCurrentPosition(
     (position) => {
       setLocation({
@@ -11,21 +12,28 @@ const BeautifulMap = () => {
         long: position.coords.longitude,
       });
     },
-    (error) => console.log(error)
+    (error) => {
+      console.log(error);
+    }
   );
-  return (
-    <>
-      <Map
-        center={{ lat: location.lat, lng: location.long }}
-        className="map-container"
-      >
-        <MapMarker
-          className="myPos"
-          position={{ lat: location.lat, lng: location.long }}
-        />
-      </Map>
-    </>
-  );
+
+  if (location.lat !== 0 && location.long !== 0) {
+    return (
+      <>
+        <Map
+          center={{ lat: location.lat, lng: location.long }}
+          className="map-container"
+        >
+          <MapMarker
+            className="myPos"
+            position={{ lat: location.lat, lng: location.long }}
+          />
+        </Map>
+      </>
+    );
+  } else {
+    return <Loading />;
+  }
 };
 
 export default BeautifulMap;
